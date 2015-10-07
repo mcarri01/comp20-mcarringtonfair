291e5f6a613291dc9ca411e6ee2a549313dd1e66
developer <matthew.carrington_fair@tufts.edu>
2015-10-07 00:36:29 -0400

    uploading changes to game.js that add background image and birds to canvas

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,25 @@
-// Replace this with your code...
+function init() {
+	var canvas = document.getElementById('game_canvas');
+	var ctx = canvas.getContext('2d');
+	
+	var background = new Image();
+	background.src = 'duckhunt-background.gif';
+	background.alt = 'background_image';
+	background.addEventListener("load", function(){
+	ctx.drawImage(background, 0, 0);
+	}, false);
+	
+	var first_bird = new Image();
+	first_bird.src = 'duckhunt_various_sheet.png';
+	first_bird.alt = 'first_bird';
+	first_bird.addEventListener("load", function(){
+	ctx.drawImage(first_bird, 0, 120, 35, 30, 100, 100, 35, 30);
+	}, false);
+	
+	var second_bird = new Image();
+	second_bird.src = 'duckhunt_various_sheet.png';
+	second_bird.alt = 'second_bird';
+	second_bird.addEventListener("load", function(){
+	ctx.drawImage(second_bird, 172, 157, 31, 31, 150, 80, 31, 31);
+	}, false);	
+}
