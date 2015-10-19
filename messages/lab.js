6a0e3bf1c1b4b90ca6850d541a880537ffd85a38
developer <matthew.carrington_fair@tufts.edu>
2015-10-19 01:18:36 -0400

    testing whether json was parsed

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,7 @@
-// Your JavaScript goes here...
+function parse(){
+	var data = new XMLHttpRequest;
+	data.open("GET", "data.json", true);
+	var mes = JSON.parse(data.json);
+	var messages = document.getElementById('messages');
+	console.log(messages['id']);
+}
