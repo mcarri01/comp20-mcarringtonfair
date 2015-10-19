844effebe11e6e6e2c973ebd1c1263cd4f399b98
mcarri01 <matthew.carrington_fair@tufts.edu>
2015-10-19 03:00:38 -0400

    matching gh-pages and master branch

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here...
+function parse(){
+	var data = new XMLHttpRequest;
+	data.onreadystatechange = function(){
+		if (data.readyState == 4 && data.status == 200){
+			var message = JSON.parse(data.responseText);
+			send(message);
+			
+			}
+		}	
+	data.open("GET", "data.json", true);
+	data.send();
+}
+function send(message){
+	var mes = document.getElementById('messages');
+	var text = "";
+	for (var i = 0; i < message.length; i++){
+		text += message[i]['id'] + " " + message[i]['content'] + " "
+			+ message[i]['username'] + "<br>";
+	}
+	mes.innerHTML = text;
+}
