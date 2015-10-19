38a876fa93f514780c2ddf8114ccb6f6f244ca7d
developer <matthew.carrington_fair@tufts.edu>
2015-10-19 02:14:24 -0400

    syntax error, stil trying for previous commit statement

--- messages/lab.js
+++ messages/lab.js
@@ -15,6 +15,8 @@ function parse(){
 function send(message){
 	var mes = document.getElementById('messages');
 	mes.innerHTML = message[1]['id'];
+	mes.innerHTML = message[1]['content'];
+	mes.innerHTML = message[1]['username'];
 	
 	
 }
